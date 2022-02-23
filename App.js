36d03f5cae29f1cb8d46c4ad51dc2a6bea763f1a
Binit Chandra Jha <binitchandrajha@Binits-MacBook-Pro.local>
2022-02-23 20:37:14 +0545

    snap-carousel integrated

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Dimensions, Image} from 'react-native';
 import CircularSlider from './src/CircularSlider';
+import Carousel from 'react-native-snap-carousel';
+
+import {IMAGES} from './src/images';
 
 class CircularSliderApp extends Component {
   constructor(props) {
@@ -9,19 +12,42 @@ class CircularSliderApp extends Component {
       slider1: 10,
     };
   }
+  _renderItem({item}) {
+    return <Image source={item?.url} />;
+  }
   render() {
     return (
-      <View style={styles.root}>
-        <View style={styles.container}>
-          <View style={styles.slider1}>
-            <CircularSlider
-              width={400}
-              height={400}
-              meterColor="#0cd"
-              textColor="#fff"
-              value={this.state.slider1}
-              onValueChange={value => this.setState({slider1: value})}
-            />
+      <View style={{flex: 1}}>
+        <View style={{height: 300}}>
+          <Carousel
+            ref={c => {
+              this._carousel = c;
+            }}
+            data={IMAGES}
+            renderItem={this._renderItem}
+            sliderWidth={400}
+            itemWidth={300}
+          />
+        </View>
+
+        {/* <Image source={require('./assets/images/1.png')} /> */}
+
+        <View style={styles.root}>
+          <View style={styles.container}>
+            <View style={styles.slider1}>
+              <CircularSlider
+                width={400}
+                height={400}
+                meterColor="#0cd"
+                textColor="#fff"
+                value={this.state.slider1}
+                onValueChange={value =>
+                  this.setState({slider1: value}, () => {
+                    this._carousel.snapToItem(value);
+                  })
+                }
+              />
+            </View>
           </View>
         </View>
       </View>
@@ -32,14 +58,14 @@ class CircularSliderApp extends Component {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#fff',
   },
   container: {
     position: 'relative',
     width: 400,
-    height: 400,
+    height: 100,
   },
   slider1: {
     position: 'absolute',

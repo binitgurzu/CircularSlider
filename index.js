28f42286c192ec837ce2faadf4c12271a807f221
Binit Chandra Jha <binit@gurzu.com>
2022-04-18 15:27:54 +0545

    circular package copy file created

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+//import App from './App';
 //import App from './src/second-circular-slider';
 //import App from './src/circular-package/Main';
+import App from './src/circular-package-copy/Main';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);

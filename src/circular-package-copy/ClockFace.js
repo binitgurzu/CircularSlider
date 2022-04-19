92959d79d5296980692e4e6aed54fc74289d15f1
Binit Chandra Jha <binit@gurzu.com>
2022-04-19 09:20:25 +0545

    apply linear gradient inside circle

--- src/circular-package-copy/ClockFace.js
+++ src/circular-package-copy/ClockFace.js
@@ -1,5 +1,15 @@
 import React, {PureComponent} from 'react';
-import {G, Circle, Text, Line, RadialGradient, Stop} from 'react-native-svg';
+import {
+  G,
+  Circle,
+  Text,
+  Line,
+  RadialGradient,
+  Stop,
+  Defs,
+  LinearGradient,
+  Ellipse,
+} from 'react-native-svg';
 import range from 'lodash.range';
 import PropTypes from 'prop-types'; // ES6
 
@@ -15,7 +25,13 @@ export default class ClockFace extends PureComponent {
     return (
       <G>
         <G>
-          <Circle cx="0" cy="0" r={r - stroke / 2} fill="#669BBA">
+          <Defs>
+            <LinearGradient id="grad" x1="0" y1="0" x2="1" y2="0">
+              <Stop offset="0" stopColor="#669BBA" stopOpacity="1" />
+              <Stop offset="1" stopColor="#125176" stopOpacity="1" />
+            </LinearGradient>
+          </Defs>
+          <Circle cx="0" cy="0" r={r - stroke / 2} fill="url(#grad)">
             <Text fill={stroke} fontSize="16" textAnchor="middle">
               Hello
             </Text>
